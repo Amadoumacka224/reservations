d701c009d1ed4d1ed442dbadf2f9854adf07afec
Amadoumacka224 <mackaldine@gmail.com>
2025-03-10 00:52:21 +0100

    Relation ManyToMany between User & Representation

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/Representation.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/Representation.java
@@ -2,8 +2,10 @@ package be.iccbxl.pid.reservationsspringboot.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Entity
 @Table(name="representations")
@@ -28,6 +30,12 @@ public class Representation {
     @JoinColumn(name="location_id", nullable=true)
     private Location location;
 
+    /**
+     * Liste des utilisateurs ayant réservé cette représentation
+     */
+    @ManyToMany(mappedBy = "representations")
+    private List<User> users = new ArrayList<>();
+
     public Representation() { }
 
     public Representation(Show show, LocalDateTime when, Location location) {
@@ -48,10 +56,31 @@ public class Representation {
         this.location = location;
     }
 
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public Representation addUser(User user) {
+        if(!this.users.contains(user)) {
+            this.users.add(user);
+            user.addRepresentation(this);
+        }
+
+        return this;
+    }
+
+    public Representation removeUser(User user) {
+        if(this.users.contains(user)) {
+            this.users.remove(user);
+            user.getRepresentations().remove(this);
+        }
+
+        return this;
+    }
+
     @Override
     public String toString() {
         return "Representation [id=" + id + ", show=" + show + ", when=" + when
                 + ", location=" + location + "]";
     }
-
 }
